b559958cd154392677bf350ea009fd8ab2221281
Derek Huynen <derek.huynen@neudesic.com>
2024-03-15 21:37:43 -0700

    Add project files.

---
+++ kernal-test/Program.cs
@@ -0,0 +1,67 @@
+﻿using kernal_test;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddAzureOpenAIChatCompletion(
+         "gpt-35-turbo",                      // Azure OpenAI Deployment Name
+         "", // Azure OpenAI Endpoint
+         "");      // Azure OpenAI Key
+
+// Alternative using OpenAI
+//builder.AddOpenAIChatCompletion(
+//         "gpt-3.5-turbo",                  // OpenAI Model name
+//         "...your OpenAI API Key...");     // OpenAI API Key
+
+
+builder.Plugins.AddFromType<AuthorEmailPlanner>();
+builder.Plugins.AddFromType<EmailPlugin>();
+
+var kernel = builder.Build();
+
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory chatMessages = new ChatHistory("""
+You are a friendly assistant who likes to follow the rules. You will complete required steps
+and request approval before taking any consequential actions. If the user doesn't provide
+enough information for you to complete a task, you will keep asking questions until you have
+enough information to complete the task.
+""");
+
+// Start the conversation
+while (true)
+{
+    // Get user input
+    System.Console.Write("User > ");
+    chatMessages.AddUserMessage(Console.ReadLine()!);
+
+
+    // Get the response from the assistant
+    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+    {
+        
+    };
+
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        chatMessages,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Stream the results
+    string fullMessage = "";
+    await foreach (var content in result)
+    {
+        if (content.Role.HasValue)
+        {
+            System.Console.Write("Assistant > ");
+        }
+        System.Console.Write(content.Content);
+        fullMessage += content.Content;
+    }
+    System.Console.WriteLine();
+
+    // Add the message from the agent to the chat history
+    chatMessages.AddAssistantMessage(fullMessage);
+}
